f5b87fd349ab35eb6d65bdb9a7c797fad540dbad
Nikolaj Skrabec <skrabets@gmail.com>
2025-01-02 12:02:26 +0100

    First test

--- src/test/java/com/intellij/ide/starter/examples/Setup.kt
+++ src/test/java/com/intellij/ide/starter/examples/Setup.kt
@@ -20,6 +20,7 @@ class Setup {
 
   companion object {
     init {
+      System.setProperty("starter.debug", "true")
       di = DI {
         extend(di)
         bindSingleton<IdeInstallerFactory>(overrides = true) { createInstallerFactory() }
@@ -39,16 +40,16 @@ class Setup {
       }
     }
 
-    private val LOCAL_PATH = System.getProperty("user.home")
+    //private val LOCAL_PATH = System.getProperty("user.home")
 
     // ----- CONFIGURATION SECTION ----
-    private val PATH_TO_INSTALLED_IDE = "C:\\Program Files\\JetBrains\\PyCharm Community Edition 2023.1.2\\bin"
+    private val PATH_TO_INSTALLED_IDE = "C:/Program Files/JetBrains/PyCharm Community Edition 2023.1.2"
     const val PROJECT_LOCATION = "C:/Users/skrab/PycharmProjects/getting-started-python"
-    private val CONFIG_PATH = Paths.get(System.getProperty("user.home"), "AppData", "Roaming", "JetBrains", "PyCharmCE2023.1")
-    private val PLUGINS_PATH = Paths.get(System.getProperty("user.home"), "AppData", "Roaming", "JetBrains", "PyCharmCE2023.1", "plugins")
+    private val CONFIG_PATH = Paths.get("C:/Users/skrab/AppData/Roaming/JetBrains/PyCharmCE2023.1")
+    private val PLUGINS_PATH = Paths.get("$CONFIG_PATH/plugins")
 
     //      pick IDE type between "IC" for Community edition and "IU" for Ultimate Edition
-    val IDE_TYPE = IdeProductProvider.IC
+    val IDE_TYPE = IdeProductProvider.PY
 
     /**
      * Sets up the test context by initializing the necessary objects and configurations.
--- src/test/java/com/intellij/ide/starter/examples/junit5/PyCharmGitHubProject.kt
+++ src/test/java/com/intellij/ide/starter/examples/junit5/PyCharmGitHubProject.kt
@@ -2,20 +2,16 @@ package com.intellij.ide.starter.examples.junit5
 
 import com.intellij.ide.starter.examples.Setup
 import com.intellij.ide.starter.ide.IDETestContext
-import com.intellij.ide.starter.ide.IdeProductProvider
-import com.intellij.ide.starter.junit5.hyphenateWithClass
-import com.intellij.ide.starter.models.TestCase
-import com.intellij.ide.starter.runner.CurrentTestMethod
-import com.intellij.ide.starter.runner.Starter
-import com.intellij.ide.starter.project.LocalProjectInfo
 import com.intellij.tools.ide.performanceTesting.commands.CommandChain
 import com.intellij.tools.ide.performanceTesting.commands.exitApp
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import java.nio.file.Paths
+import org.slf4j.LoggerFactory
 
 class PyCharmGitHubTest {
     companion object {
+        private val logger = LoggerFactory.getLogger(PyCharmGitHubTest::class.java)
+
         private lateinit var context: IDETestContext
 
         @BeforeAll
